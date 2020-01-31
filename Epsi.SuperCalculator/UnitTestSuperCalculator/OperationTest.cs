806f784954a90f2becdc92315c24d5a04516a3b7
imad <imad@gmail.com>
2020-01-31 15:46:11 +0100

    references test-prjt

--- Epsi.SuperCalculator/UnitTestSuperCalculator/OperationTest.cs
+++ Epsi.SuperCalculator/UnitTestSuperCalculator/OperationTest.cs
@@ -5,9 +5,16 @@ namespace UnitTestSuperCalculator
     [TestClass]
     public class OperationTest
     {
+        /// <summary>
+        /// Vérifie que l'on renvoie 4 quand le nombre1 vaut 1 et nombre2 vaut 3
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddition()
         {
+            var op = new Operation();
+            var result = operation.addition(1, 3);
+            const int expected = 4;
+            Check.That(result).Equals(expected);
         }
     }
 }
