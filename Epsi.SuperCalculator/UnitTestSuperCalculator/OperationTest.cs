5f07ce4a990861f9ced70d8d24ee8e4eca94f781
IPlayers <max7338.m@gmail.com>
2020-01-31 16:17:31 +0100

    OperationTest for conflicts resolution

--- Epsi.SuperCalculator/UnitTestSuperCalculator/OperationTest.cs
+++ Epsi.SuperCalculator/UnitTestSuperCalculator/OperationTest.cs
@@ -5,34 +5,5 @@ namespace UnitTestSuperCalculator
     [TestClass]
     public class OperationTest
     {
-        [TestMethod]
-        public void AdditionTest()
-        {
-
-        }
-
-        [TestMethod]
-        public void SoustractionTest()
-        {
-
-        }
-
-        [TestMethod]
-        public void MultiplicationTest()
-        {
-
-        }
-
-        [TestMethod]
-        public void DivisionTest()
-        {
-
-        }
-
-        [TestMethod]
-        public void GetOperationTest()
-        {
-
-        }
     }
 }
